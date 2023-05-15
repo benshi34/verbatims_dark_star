ab0f09ee6f9fc3f3faedd927cc8c554eb6cda9b3
benshi <qbshi@princeton.edu>
2023-05-14 22:52:05 -0400

    asdf

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const HomeScreen = () => (
+  <View style={styles.screen}>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+HomeScreen.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="home" size={24} color={tintColor} />
+  ),
+  tabBarAccessibilityLabel: 'Home Tab',
+};
+
+const ProfileScreen = () => (
+  <View style={styles.screen}>
+    <Text>Profile Screen</Text>
+  </View>
+);
+
+ProfileScreen.navigationOptions = {
+  tabBarLabel: 'Profile',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="gear" size={24} color={tintColor} />
+  ),
+  tabBarAccessibilityLabel: 'Profile Tab',
+};
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sora lee is so hot</Text>
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -17,4 +58,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 12,
+    marginBottom: 4,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  tab: {
+    paddingTop: 8,
+  },
+  bar: {
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    backgroundColor: '#fff',
+  }, 
 });

d625c0d83ab66565821d1ab89e84868564881164
benshi <qbshi@princeton.edu>
2023-05-14 23:23:15 -0400

    sdf

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';

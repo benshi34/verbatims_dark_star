74a625d2c65b43cc442606f328172288dd533612
RishiVirpura <rishivirpura@gmail.com>
2023-05-17 23:04:09 -0400

    CHanges to settings screen

--- pages/SettingScreen.js
+++ pages/SettingScreen.js
@@ -3,23 +3,35 @@ import { View, Text, TouchableOpacity, StyleSheet, Button, SafeAreaView } from "
 const SettingScreen = ({ navigation }) => {
     return (
       <View style ={styles.container}>
-        <View style={styles.header}>
-        <Text style={styles.headerText}>Settings</Text>
-        </View>
-
         <View
-      style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
+      style={{ flex: 1, display: 'flex' }}
     >
-      <TouchableOpacity style={{ backgroundColor: '#147EFB', padding: 10, borderRadius:4 }}>
-        <Text style={{ color: '#fff' }}>Profile</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Profile</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{ backgroundColor: '#147EFB', padding: 10, borderRadius:4 }}>
-        <Text style={{ color: '#fff' }}>Friends</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Friends</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={{ backgroundColor: '#147EFB', padding: 10, borderRadius:4 }}>
-        <Text style={{ color: '#fff' }}>Verbatims Premium</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Verbatims Premium</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Password</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Privacy</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Notifications</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>Help</Text>
       </TouchableOpacity>
 
     </View>
@@ -28,43 +40,21 @@ const SettingScreen = ({ navigation }) => {
     };
 
     const styles = StyleSheet.create({
+      button:{
+        backgroundColor: '#147EFB', 
+        padding: 20, 
+        borderRadius:4, 
+        borderColor: 'black', 
+        borderWidth: 1
+      },
+      text:{
+        color: '#fff', 
+        fontSize: 20, 
+        fontWeight: 'bold'
+      },
       container: {
         flex: 1,
         backgroundColor: '#fff',
-      },
-      header: { 
-        padding: 50, 
-        alignItems: 'center',
-        justifyContent: 'center', 
-        marginBottom: 100,
-      },
-      headerText: {
-        fontSize: 20,
-        color: 'blue',
-        fontWeight: 'bold',
-      },
-      label: {
-        marginTop: 50,
-        marginLeft: 100,
-      },
-      input: {
-        borderWidth: 1, 
-        borderColor: '#777',
-        padding: 8,
-        margin: 10,
-        width: 200, 
-        borderRadius: 10,
-      },
-      buttonContainer: {
-        marginTop: 20,
-        marginBottom: 20,
-        backgroundColor: 'orange',
-        alignItems: 'center',
-        justifyContent: 'center', 
-        borderRadius: 4
-      },
-      IDFK: {
-        alignItems: 'center',
       }
     }); 
 export default SettingScreen

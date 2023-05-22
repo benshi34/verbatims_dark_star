511642ae7afe8401eaa370954ce91c0595029a34
benshi <qbshi@princeton.edu>
2023-05-21 22:44:21 -0400

    AddScreen

---
+++ pages/AddScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+
+const AddScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text>AddScreen</Text>
+        </View>
+    )
+}
+
+export default AddScreen

843fdb551945a80f33a41645be042026b79aa722
benshi <qbshi@princeton.edu>
2023-05-28 22:25:29 -0400

    HomeScreen Firebase Integration

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,39 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity, ScrollView, TextInput, Modal} from 'react-native';
+import { getDatabase, ref, get, onValue } from "firebase/database";
+
+import { app } from "../Firebase.js";
 
 const HomeScreen = ({ navigation }) => {
     const [discussionPosts, setDiscussionPosts] = useState([]);
+    const [selectedPost, setSelectedPost] = useState(null);
+    const [commentText, setCommentText] = useState('');
+    const [showModal, setShowModal] = useState(false);
+
+    const db = getDatabase(app);
 
     useEffect(() => {
       // Simulated data for discussion posts
-      const dummyData = [
-        {
-          id: 1,
-          user: 'John',
-          post: 'Wanna Fuck?',
-          profilePic: require('../assets/kharn.jpg'),
-          isFavorite: false,
-          likes: 0,
-        },
-        {
-          id: 2,
-          user: 'Sarah',
-          post: 'Ew wtf why would you say something like that? ',
-          profilePic: require('../assets/kharn.jpg'),
-          isFavorite: false,
-          likes: 0,
-        },
-        {
-          id: 3,
-          user: 'Michael',
-          post: 'I would like to fuck.',
-          profilePic: require('../assets/kharn.jpg'),
-          isFavorite: false,
-          likes: 0,
-        },
-      ];
-  
-      setDiscussionPosts(dummyData);
+      const fetchDiscussionPosts = async () => {
+        try {
+          const dbref = ref(db, "Verbatims")
+          onValue(dbref, (snapshot) => {
+            data = snapshot.val()
+            if (data) {
+              const discussionPostsArray = Object.keys(data).map((key) => {
+                return { id: key, ...data[key] };
+              });
+              setDiscussionPosts(discussionPostsArray);
+            }
+          })
+        } catch (error) {
+          console.error('Error fetching discussion posts: ', error);
+        }
+      }
+      fetchDiscussionPosts();
     }, []);
     
     const toggleFavorite = (postId) => {
@@ -47,12 +44,37 @@ const HomeScreen = ({ navigation }) => {
     const toggleLike = (postId) => {
       setDiscussionPosts((prevPosts) =>
         prevPosts.map((post) =>
-          post.id === postId ? { ...post, likes: post.likes + 1 } : post
+          post.id === postId ? { ...post, isLiked: !post.isLiked} : post
         )
       );
     };
 
+    const addComment = () => {
+      if (commentText.trim()) {
+        setDiscussionPosts((prevPosts) =>
+          prevPosts.map((post) =>
+            post.id === selectedPost.id ? { ...post, comments: [...post.comments, commentText] } : post
+          )
+        );
+        setCommentText('');
+      }
+    };
+
+    const openModal = (postId) => {
+      const post = discussionPosts.find((post) => post.id === postId);
+      setSelectedPost(post);
+      setShowModal(true);
+    };
+  
+    const closeModal = () => {
+      setSelectedPost(null);
+      setShowModal(false);
+    };
+  
+
     const renderDiscussionPost = ({ item }) => {
+      const isLiked = item.isLiked ? styles.likeButtonLiked : null;
+
       return (
         <View style={styles.postContainer}>
           <View style={styles.userContainer}>
@@ -70,9 +92,14 @@ const HomeScreen = ({ navigation }) => {
               {item.isFavorite ? 'Unfavorite' : 'Favorite'}
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.likeButton} onPress={() => toggleLike(item.id)}>
-            <Text style={styles.likeButtonText}>Like ({item.likes})</Text>
+          <View style={styles.actionsContainer}>
+          <TouchableOpacity style={[styles.likeButton, isLiked]} onPress={() => toggleLike(item.id)}>
+            <Text style={styles.likeButtonText}>Like</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.commentButton} onPress={() => openModal(item.id)}>
+            <Text style={styles.commentButtonText}>View Comments</Text>
+          </TouchableOpacity>
+          </View>
         </View>
       );
     };
@@ -80,31 +107,51 @@ const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Verbatims</Text>
-                <ScrollView contentContainerStyle={styles.scrollContainer}>
-                  <FlatList
-                  data={discussionPosts}
-                  renderItem={renderDiscussionPost}
-                  keyExtractor={(item) => item.id.toString()}
-                  contentContainerStyle={styles.listContainer}
+              <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <FlatList
+                data={discussionPosts}
+                renderItem={renderDiscussionPost}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={styles.listContainer}
+                />
+              </ScrollView>
+              {selectedPost && (
+                <Modal visible={showModal} animationType="slide" transparent>
+                  <View style={styles.modalContainer}>
+                  <View style={styles.modalContent}>
+                  <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+                    <Text style={styles.closeButtonText}>Close</Text>
+                  </TouchableOpacity>
+
+                  <Text style={styles.modalPost}>{selectedPost.post}</Text>
+
+                  <View style={styles.commentsContainer}>
+                    <Text style={styles.commentsHeading}>Comments:</Text>
+                    {selectedPost.comments.map((comment, index) => (
+                      <Text key={index} style={styles.commentText}>{comment}</Text>
+                    ))}
+                  </View>
+                  <TextInput
+                    style={styles.commentInput}
+                    placeholder="Add a comment..."
+                    onChangeText={(text) => setCommentText(text)}
+                    value={commentText}
+                    onSubmitEditing={addComment}
                   />
-                </ScrollView>
-        </View>
+                </View>
+                </View>
+              </Modal>
+              )}
+      </View>
     );
 }
-  
-HomeScreen.navigationOptions = {
-    tabBarLabel: 'Home',
-    tabBarIcon: ({ tintColor }) => (
-        <Icon name="home" size={24} color={tintColor} />
-    ),
-    tabBarAccessibilityLabel: 'Home Tab',
-};
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 16,
       backgroundColor: '#fff',
+      paddingTop: 40,
     },
     header: {
       fontSize: 24,
@@ -136,8 +183,10 @@ const styles = StyleSheet.create({
     },
     postTextContainer: {
       flex: 1,
+      marginBottom: 8,
     },
     username: {
+      fontSize: 16,
       fontWeight: 'bold',
     },
     postText: {
@@ -145,40 +194,103 @@ const styles = StyleSheet.create({
     },
     favoriteButton: {
       position: 'absolute',
-      top: 0,
-      right: 0,
-      padding: 8,
-      borderRadius: 20,
-      backgroundColor: '#e6e6e6',
-      alignItems: 'center',
-      justifyContent: 'center',
+      top: 8,
+      right: 8,
+      paddingHorizontal: 12,
+      paddingVertical: 6,
+      borderRadius: 16,
+      backgroundColor: '#e1e1e1',
     },
     favoriteButtonText: {
-      fontSize: 12,
-      fontWeight: 'bold',
+      fontSize: 14,
       color: '#333',
     },
     favoriteButtonActive: {
       backgroundColor: '#ffcc00',
     },
-    favoriteButtonTextActive: {
-      color: '#fff',
-    },
     likeButton: {
-      marginTop: 8,
-      padding: 8,
-      borderRadius: 4,
-      backgroundColor: '#e6e6e6',
-      alignItems: 'center',
-      justifyContent: 'center',
+      paddingHorizontal: 12,
+      paddingVertical: 6,
+      borderRadius: 16,
+      backgroundColor: '#e1e1e1',
+    },
+    likeButtonLiked: {
+      backgroundColor: 'red',
     },
     likeButtonText: {
-      fontSize: 12,
-      fontWeight: 'bold',
+      fontSize: 14,
       color: '#333',
     },
     scrollContainer: {
+      flexGrow: 1,
       paddingBottom: 16,
+    },
+    actionsContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    commentButton: {
+      paddingHorizontal: 12,
+      paddingVertical: 6,
+      borderRadius: 16,
+      backgroundColor: '#e1e1e1',
+    },
+    closeButton: {
+      alignSelf: 'flex-end',
+      backgroundColor: '#ddd',
+      paddingVertical: 8,
+      paddingHorizontal: 12,
+      borderRadius: 8,
+    },
+    closeButtonText: {
+      fontSize: 14,
+      color: '#333',
+    },
+    commentsHeading: {
+      paddingHorizontal: 12,
+      paddingVertical: 12,
+    },
+    commentInput: {
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 8,
+      padding: 8,
+      marginBottom: 16,
+    },
+    commentList: {
+      maxHeight: 200,
+    },
+    comment: {
+      marginBottom: 8,
+    },
+    commentText: {
+      flexDirection: 'row',
+      marginBottom: 12,
+      alignItems: 'center',
+    },
+    commentButtonText: {
+      fontSize: 14,
+      color: '#333',
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    modalContent: {
+      backgroundColor: '#fff',
+      borderRadius: 8,
+      width: '100%',
+      height: '70%',
+      paddingHorizontal: 16,
+      paddingTop: 16,
+    },
+    commentsContainer: {
+      marginTop: 16,
+      borderTopWidth: 1,
+      borderTopColor: '#ccc',
+      paddingTop: 16,
     },
   });
 
